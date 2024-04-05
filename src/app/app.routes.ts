47c2b55fcc299c753b6ae1460ae4b824687c8dfd
assoul <untitled>
2024-04-05 14:33:53 +0100

    commit by untitled3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
+import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { ContactComponent } from './contact/contact.component';
 import { Routes } from '@angular/router';
+const routes: Routes = [
+  { path: 'contact', component: ContactComponent }
+];
 
-export const routes: Routes = [];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+
+
